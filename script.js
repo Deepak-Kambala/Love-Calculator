f97e626295bf2ff22c988d04d756dd53b1e00957
Samiksha-bajoria <samikshabajoria2006@gmail.com>
2025-10-12 22:39:41 +0530

    adding scroll to top button

--- script.js
+++ script.js
@@ -1772,3 +1772,22 @@ document.addEventListener('DOMContentLoaded', () => {
                 });
             }
         });
+	// Scroll to Top Button
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollToTopBtn.classList.add("show");
+  } else {
+    scrollToTopBtn.classList.remove("show");
+  }
+});
+
+scrollToTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
+	
